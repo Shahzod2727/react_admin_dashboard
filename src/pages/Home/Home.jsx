9fbd32cac4f43d8d616c90560e91fe8e1abb8914
Shahzod2727 <shahzodjava@gmail.com>
2022-03-25 21:20:04 +0500

    part homeWidgets

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import FeaturedInfo from "../../components/FeaturedInfo/FeaturedInfo";
 import Chart from "../../components/Chart/Chart";
+import WidgetSm from "../../components/WidgetSm/WidgetSm";
+import WidgetLg from "../../components/WidgetLg/WidgetLg";
 import { userData } from "../../dummyData";
 import "./Home.css";
+
+
+
 export default function Home() {
   return (
     <div className="home">
-      {" "}
       <FeaturedInfo />
       <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+      <div className="homeWidgets">
+        <WidgetSm/>
+        <WidgetLg/>
+      </div>
     </div>
-  );
+  ) ;
 }
