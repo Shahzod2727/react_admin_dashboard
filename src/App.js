4866601fef4e079dd8c4d5548150df6ffabb2c3f
Shahzod2727 <shahzodjava@gmail.com>
2022-03-25 11:32:52 +0500

    part 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 // import Topbar from "./components/Topbar/Topbar";
 import Topbar from "./components/Topbar/Topbar";
-
+import Sidebar from "./components/Sidebar/Sidebar";
+import "./app.css";
+import React from "react";
 function App() {
   return (
     <div className="App">
       <Topbar />
+      <div className="container">
+        <Sidebar />
+        <div className="others">other pages</div>
+      </div>
     </div>
   );
 }
--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,11 +1,30 @@
 import React from "react";
-import './topbar.css'
+import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+// import Img from '../images/'
+import "./Topbar.css";
 export default function Topbar() {
   return (
     <div className="topbar">
       <div className="topbarWrapper">
-        <div className="topLeft"><span className="logo">Logo</span></div>
-        <div className="topRight"><div className="topbarIcons"></div></div>
+        <div className="topLeft">
+          <span className="logo">Logo</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconsContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+
+          <div className="topbarIconsContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+
+          <div className="topbarIconsContainer">
+            <Settings />
+          </div>
+          <img src="https://picsum.photos/200/300" alt="" className="topAvatar" />
+        </div>
       </div>
     </div>
   );
