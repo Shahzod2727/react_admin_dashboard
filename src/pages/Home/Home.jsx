bbcf3b75405d31483855e4dee880694df1e319e1
Shahzod2727 <shahzodjava@gmail.com>
2022-03-25 12:58:19 +0500

    part chart

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import FeaturedInfo from "../../components/FeaturedInfo/FeaturedInfo";
+import Chart from "../../components/Chart/Chart";
+import { userData } from "../../dummyData";
 import "./Home.css";
 export default function Home() {
   return (
     <div className="home">
       {" "}
       <FeaturedInfo />
+      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
     </div>
   );
 }
